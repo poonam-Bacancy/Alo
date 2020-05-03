06c86982065548418d586c7a9da6086ac53fe0fb
poonam-Bacancy <poonam.shah@bacancytechnology.com>
2020-05-03 22:56:22 +0530

    Theme integration and modified the signup functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
+# Application Controller
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
   protected
 
   def after_sign_in_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
   get 'home/index'
+  root to: 'home#landing_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root to: "home#index"
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords' }
 
   devise_scope :user do
-    root to: "users/sessions#new"
+    # root to: "users/sessions#new"
     # get 'users/callback', to: 'users/sessions#callback'
   end
 end
